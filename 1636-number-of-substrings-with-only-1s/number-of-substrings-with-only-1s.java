1b64128a33dcff9919a7dd377d19e99940d56adb
SUBHIKSHA . N <subibcloud1108@gmail.com>
2025-11-16 08:19:58 +0530

    Time: 6 ms (23.30%) | Memory: 46.4 MB (9.16%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -0,0 +1,15 @@
+class Solution {
+    int mod=(int)1e9+7;
+    public int numSub(String s) {
+        int n=s.length();
+        //0 1 1 0 1 1 1
+        //0 2 1 0 3 2 1
+        long totalCount=0, count=0;
+        for(int i=n-1; i>=0; i--){
+            if(s.charAt(i)=='1') count=(count+1)%mod;
+            else count=0;
+            totalCount=(totalCount+count)%mod;
+        }
+        return (int)totalCount%mod;
+    }
+}
