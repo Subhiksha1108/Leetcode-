d260cbb0e53cb942009fa51533976c062115a275
SUBHIKSHA . N <subibcloud1108@gmail.com>
2026-01-26 15:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (10.18%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+      
+        ListNode  Slow=head;
+        ListNode Fast=head;
+        while(Fast!=null && Fast.next!=null)
+        {
+            Slow=Slow.next;
+            Fast=Fast.next.next;
+        }
+        return Slow;
+    }
+}
