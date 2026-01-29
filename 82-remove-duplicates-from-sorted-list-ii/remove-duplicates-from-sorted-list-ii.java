b5120ba08dbbcf24e175eb5830563313e7244a5d
SUBHIKSHA . N <subibcloud1108@gmail.com>
2026-01-29 10:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (45.99%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,36 @@
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode curr = head;  
+        
+        ListNode prev = null; 
+        
+        if(head == null){
+            return null;
+        }
+
+        while(curr != null ){
+            if(curr.next != null && curr.val == curr.next.val){
+                
+                int val = curr.val ; 
+                while(curr!= null && curr.val == val ){
+                    curr = curr.next; 
+                }
+                if(prev != null){
+                    prev.next = curr; 
+                }
+                else{
+                    head = curr ; 
+                }
+            }
+            else{
+                prev = curr ; 
+                curr = curr.next; 
+            }
+
+
+        }
+        return head; 
+    }
+}
