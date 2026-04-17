7ce0b27d79cd29bb5bada5182beccdee3377a482
SUBHIKSHA . N <subibcloud1108@gmail.com>
2026-04-17 16:36:48 +0530

    Time: 1 ms (63.74%) | Memory: 43.1 MB (58.88%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(char j: jewels.toCharArray())
+        {
+            for(char s:stones.toCharArray())
+            {
+                if(j==s)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
